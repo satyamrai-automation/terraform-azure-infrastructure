0d524c9c34b6d709515e391b5b1d36d376973073
satyamrai-automation <satyam636rai@gmail.com>
2026-05-26 12:50:32 +0530

    add two file

--- provid.tf
+++ provid.tf
@@ -2,11 +2,7 @@ terraform {
     required_providers {
       azurerm = {
         source = "hashicorp/azurerm"
-        version = "4.71.0"
+        version = "4.70.0"
       }
     }
-}
-
-provider "azurerm" {
-    features{}
 }
